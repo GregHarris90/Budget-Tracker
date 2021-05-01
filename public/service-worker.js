ebf198b8f9ed80a9667e29b38be10928b0c8148c
Greg Harris <ghart.productions@gmail.com>
2021-04-30 22:01:03 -0700

    added comments to service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+// files to cache
 const FILES_TO_CACHE = [
     '/',
     'index.html',
@@ -8,5 +9,18 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
 ];
 
+// cache names for static files & cache data
 const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
+
+// install Service Worker
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Your files were cached successfully!');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
